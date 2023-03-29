c1a9c005d1a24a0170932a8da8fdee38109eb39b
mk2986 <129020502+mk2986@users.noreply.github.com>
2023-03-29 10:24:52 +0900

    GitHub for git pull

--- hello.py
+++ hello.py
@@ -2,9 +2,8 @@
 // First: GitHub modification
 // Second: Local repository modification
 
-
 print("Hello world")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
-
+print("Tell my world")
